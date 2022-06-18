692fb8b851205b719e2083d9d405d48f79661263
Gabriel Venancio <Gabriel@DESKTOP-QQFFJ0U>
2022-06-18 19:26:44 -0300

    Feat: create Product and Purchase entities

---
+++ GB.ApiDotNet6.Domain/Entities/Purchase.cs
@@ -0,0 +1,42 @@
+﻿using GB.ApiDotNet6.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB.ApiDotNet6.Domain.Entities
+{
+    public class Purchase
+    {
+        public int Id { get; private set; }
+        public int ProductId { get; private set; }
+        public int PersonId { get; private set; }
+        public DateTime Date { get; private set; }
+        public Person Person { get; set; }
+        public Product Product { get; set; }
+
+        public Purchase(int productId, int personId, DateTime? date)
+        {
+            Validation(productId, personId, date);
+        }
+
+        public Purchase(int id, int productId, int personId, DateTime? date)
+        {
+            DomainValidationException.When(Id < 0, "Id deve ser informado!");
+            Id = id;
+            Validation(productId, personId, date);
+        }
+
+        private void Validation(int productId, int personId, DateTime? date)
+        {
+            DomainValidationException.When(productId < 0, "Produto deve ser informado!");
+            DomainValidationException.When(personId < 0, "Pessoa deve ser informada!");
+            DomainValidationException.When(!date.HasValue, "Data da compra deve ser informada");
+
+            PersonId = personId;
+            ProductId = productId;
+            Date = date.Value;
+        }
+    }
+}
