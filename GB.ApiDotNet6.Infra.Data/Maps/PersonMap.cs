b32d6bca11aaca5c0fa2ab23d37efd05c768ce4c
Gabriel Venancio <Gabriel@DESKTOP-QQFFJ0U>
2022-06-18 22:45:54 -0300

    Feat: Person Mapper

---
+++ GB.ApiDotNet6.Infra.Data/Maps/PersonMap.cs
@@ -0,0 +1,39 @@
+﻿using GB.ApiDotNet6.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GB.ApiDotNet6.Infra.Data.Maps
+{
+    public class PersonMap : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.ToTable("Pessoa");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                .HasColumnName("idPessoa")
+                .UseIdentityColumn();
+
+            builder.Property(c => c.Document)
+                .HasColumnName("Documento");
+
+            builder.Property(c => c.Name)
+                .HasColumnName("Nome");
+
+            builder.Property(c => c.Phone)
+                .HasColumnName("celular");
+
+            builder.HasMany(c => c.Purchases)
+                .WithOne(p => p.Person)
+                .HasForeignKey(p => p.PersonId);
+
+        }
+    }
+}
